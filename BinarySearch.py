8837b11cde3c7ce8ca91ae0de646cd31ccca2ed4
Rani Deore <ranideore1998@gamil.com>
2020-07-13 21:42:13 +0530

    add file binarySearch

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,6 +1,6 @@
 def binarySearch(arr, l, r, x):
     while l <= r:
-        mid = l + (r-1) // 2
+        mid = l + (r-l) // 2
         if arr[mid] == x:
             return mid
         elif arr[mid] < x:
