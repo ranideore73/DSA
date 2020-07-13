096389fbed5974f960cd57844f085358f66573ac
Rani Deore <ranideore1998@gamil.com>
2020-07-13 21:10:28 +0530

    add file linearsearch

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,1 +1,19 @@
-def binarySearch()
+def binarySearch(arr, l, r, x):
+    while l <= r:
+        mid = l + (r-1) // 2
+        if arr[mid] == x:
+            return mid
+        elif arr[mid] < x:
+            l = mid + 1
+        else:
+            r = mid - 1
+    return -1
+
+arr = list(map(int, input().split()))
+x = int(input()) 
+
+result = binarySearch(arr, 0, len(arr)-1, x)
+if result != -1:
+    print("Element present at", result)
+else:
+    print("Element not found")
