899da3c317b60af03f38aa840b57034356dc5a11
Rani Deore <ranideore1998@gamil.com>
2020-07-14 17:06:20 +0530

    add file linearsearch

--- LinearSearch.py
+++ LinearSearch.py
@@ -1,12 +1,12 @@
-def search(arr, n, k):
-    for i in range(0, n):
+def search(arr, k):
+    for i in range(0, len(arr)):
         if (arr[i] == k):
             return i
     return -1
 arr = list(map(int, input().split()))
 n = len(arr)
 k = int(input())
-r = search(arr, n, k)
+r = search(arr, k)
 if (r == -1):
     print("not found")
 else:
