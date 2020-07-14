e2835fb0a0669508673065d7c8ead5ed8498cc58
Rani Deore <ranideore1998@gamil.com>
2020-07-14 17:48:06 +0530

    add file bubbleSort

---
+++ BubbleSort.py
@@ -0,0 +1,11 @@
+def bubbleSort(arr):
+	for i in range(len(arr)):
+    		for j in range(0,len(arr)-i-1):
+    				if arr[j] > arr[j+1]:
+    						arr[j], arr[j+1] = arr[j+1], arr[j]
+	return arr
+
+arr = list(map(int, input().split()))
+r = bubbleSort(arr)
+print("sorted elements are", r)
+
